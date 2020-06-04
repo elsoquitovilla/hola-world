a5e65fa3a2018cfe0b71520815c0ea2b30135aec
elsoquitovilla <oscar.villanueva2020@twk.cl>
2020-06-04 16:37:18 -0400

    asd

---
+++ holamundo.py
@@ -0,0 +1,26 @@
+saludo = " hola como estas?"
+print(saludo)
+
+respuesta = input()
+print(respuesta)
+
+print("ingresar numero")
+numero1 = int(input())
+
+numero2 = 4
+print(" el resultado es: ",numero1+numero2)
+
+edad = input("hola dime tu edad")
+
+nombre = input("Ingresa tu nombre")
+print(nombre.upper()) #pasar a mayuscula
+print(nombre.lower()) #pasar a minuscula
+
+#OPERADORES ARITMETICOS
+
++, -, * ,%, //, **
+
+
+
+
+print("holamundo")
---
+++ venv/hola mundo.py
@@ -0,0 +1,6 @@
+numero=int(input("ingrese un numero a saber su tabla de multiplicar"))
+
+print("1 X ",numero," =",1*numero)
+git config --global user.email "oscar.villanueva2020@twk.cl"
+git config --global user.name "elsoquitovilla"
+
